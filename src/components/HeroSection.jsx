448d2542a007cc0aaffb12a6779de0ff9aba9fe7
Brianengine <brianduke0097@gmail.com>
2025-01-01 17:26:40 +0800

    作品添加Button

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ const HeroSection = () => {
     return (
       <section className="hero" >
         <div className="profile-photo">
-          <img src="https://raw.githubusercontent.com/BrianTian0099/ChungTaPortfolio/refs/heads/main/src/assets/head.png?token=GHSAT0AAAAAAC4TSNYVFLYXGTSZPLHFRIFUZ3UF3QA" />
+          <img src="https://github.com/BrianTian0099/ChungTaPortfolio/blob/9d85a0907be544cbc81fabd5d0ce14d57b0c5d3e/src/assets/head.png" />
         </div>
         <div className="hero-content">
           <h1>跨領域設計師</h1>
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -7,8 +7,8 @@ import project4 from "../assets/project4.jpg";
 
 const ProjectsSection = () => {
   const projects = [
-    { title: "穿戴式裝置互動設計", description: "作品一的描述", bgImage: project1 },
-    { title: "作品二", description: "作品二的描述", bgImage: project2 },
+    { title: "穿戴式裝置互動設計", description: "作品一的描述", bgImage: project1   },
+    { title: "作品二", description: "作品二的描述", bgImage: project2  },
     { title: "作品三", description: "作品三的描述", bgImage: project3 },
     { title: "作品四", description: "作品四的描述", bgImage: project4 },
   ];
@@ -43,6 +43,7 @@ const ProjectsSection = () => {
           >
             <h2>{project.title}</h2>
             <p>{project.description}</p>
+            <button><link rel="stylesheet" href="{project.linkDrive}" />前往了解</button>
           </div>
         ))}
       </div>
