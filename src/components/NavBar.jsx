50244aa4ad34f267166506711e4b8d4caf0cd6c6
Brian <brianduke0097@gmail.com>
2025-07-11 11:48:05 +0800

    新增新增resume

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -28,11 +28,30 @@ const NavBar = () => {
       </div>
 
       <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
-        <li><a onClick={() => scrollToSection("about")}>About</a></li>
-        <li><a onClick={() => scrollToSection("skills")}>Skills</a></li>
-        <li><a onClick={() => scrollToSection("projects")}>Works</a></li>
-        <li><a onClick={() => scrollToSection("articles")}>Articles</a></li>
-        <li><a onClick={() => scrollToSection("contact")}>Contact</a></li>
+        <li>
+          <a onClick={() => scrollToSection("about")}>About</a>
+        </li>
+        <li>
+          <a onClick={() => scrollToSection("skills")}>Skills</a>
+        </li>
+        <li>
+          <a onClick={() => scrollToSection("projects")}>Works</a>
+        </li>
+        <li>
+          <a onClick={() => scrollToSection("articles")}>Articles</a>
+        </li>
+        <li>
+          <a onClick={() => scrollToSection("contact")}>Contact</a>
+        </li>
+        <li>
+          <a
+            href="https://drive.google.com/file/d/1cEdTcUsfNbKNnLB1LYWuY6Nv-6A-5g2Q/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Resume
+          </a>
+        </li>
       </ul>
     </nav>
   );
